b250816fd9d06002eacbcc4463438166e79505e6
frkodes <frkalderon@gmail.com>
2016-02-19 11:04:05 -0600

    Footer fix

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,30 +4,26 @@
 			<div class="icon-b"></div>
 		</div>
 		<div class="row">
-			<div class="col-sm-3">
+			<div class="col-sm-4 col-md-3">
 				<div class="follow-us">
 					<img src="images/siguenos.svg" alt="siguenos en redes sociales">
 				</div>
-			</div>
-			<div class="col-sm-3">
 				<ul class="social">
 					<li><a target="_blank" title="Síguenos en Facebook" href="https://www.facebook.com/PistonesGarage" class="icon-d"></a></li>
 					<li><a target="_blank" title="Síguenos en Instagram" href="https://www.instagram.com/pistonesgdl" class="icon-e"></a></li>
 					<li><a target="_blank" title="Contáctanos" href="mailto:info@pistonesgarage.com" class="icon-f"></a></li>
 				</ul>
 			</div>
-			<div class="col-sm-3">
+			<div class="col-sm-4 col-md-3 col-sm-offset-4 col-md-offset-6">
+				<div class="eleanor">
+					<img src="images/eleanor.svg" alt="Logo eleanor">
+				</div>
 				<div class="disclaimer">
 					<p>
 						Copyright &copy; <?php echo date('Y') ?> Pistones Garage <br>
 						Todos los derechos reservados <br>
 						Aviso de privacidad <a href="pdf/privacidad-pistones-hmo.pdf" target="_blank" title="Ver aviso de privacidad Hermosillo">Hmo</a> / <a href="pdf/aviso_de_privacidad_pistones.pdf" target="_blank" title="Ver aviso de privacidad Guadalajara">Gdl</a>
 					</p>
-				</div>
-			</div>
-			<div class="col-sm-3">
-				<div class="eleanor">
-					<img src="images/eleanor.svg" alt="Logo eleanor">
 				</div>
 			</div>
 		</div>
