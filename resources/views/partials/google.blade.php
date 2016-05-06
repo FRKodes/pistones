9241317e5892a8f5611246e2817d20bfa2443ecd
frkodes <frkalderon@gmail.com>
2016-05-06 10:55:52 -0500

    Addign the remarketing code

--- resources/views/partials/google.blade.php
+++ resources/views/partials/google.blade.php
@@ -6,3 +6,19 @@
   ga('create', 'UA-55515309-15', 'auto');
   ga('send', 'pageview');
 </script>
+
+<!-- Google Code para etiquetas de remarketing -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 882377907;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/882377907/?value=0&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
