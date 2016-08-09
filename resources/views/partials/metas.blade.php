bb2f51fc76a1f9d7373d881c077b0436c7468abd
frkodes <frkalderon@gmail.com>
2016-08-09 12:27:16 -0500

    Adding the google verification code and some others meta tags

--- resources/views/partials/metas.blade.php
+++ resources/views/partials/metas.blade.php
@@ -1,11 +1,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="">
-<meta name="keywords" content="">
+<meta name="google-site-verification" content="yRvAwxfLH69_RR6f3onII7VIido4z7nMsbbMjXpkZSc" />
+<meta name="description" content="!Somos el taller de los deportes con comida altamente adictiva y drinks perfectamente calibrados!.">
+<meta name="keywords" content="Burger Love, Hamburguesas Guadalajara, Servicio a domicilio, Drinks, Deportes, Sportsbar, Burgers, Chelas, Guadalajara">
 <meta name="expires" content="tue, 07 Apr 2016 09:45:00 GMT">
 <meta property="og:locale" content="es_MX">
 <meta property="og:type" content="website">
-<meta property="og:title" content="">
-<meta property="og:description" content="">
-<meta property="og:site_name" content="">
+<meta property="og:title" content="Pistones Garage">
+<meta property="og:description" content="!Somos el taller de los deportes con comida altamente adictiva y drinks perfectamente calibrados!.">
+<meta property="og:site_name" content="Pistones Garage">
 <link rel="icon" href="favicon.png" type="image/png" sizes="32x32">
 <title>@yield('titlePage', 'el taller de los deportes') - PISTONES GARAGE Food and Drink</title>
