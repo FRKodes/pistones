d1b2fd1af4723f4792ccc60aa4ee8a0c1e08a6ac
frkodes <frkalderon@gmail.com>
2017-06-09 17:49:38 -0500

    Logo fix

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -8,7 +8,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand icon-a" href="/"></a>
+			<a class="navbar-brand" href="/"><img src="images/logo_pistones_stencil.svg" alt="logo pistones stencil"></a>
 		</div>
 		<div id="navbar" class="navbar-collapse collapse">
 			<ul class="nav navbar-nav">
