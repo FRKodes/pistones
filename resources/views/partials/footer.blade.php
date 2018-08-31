3ddbd8d33679d504b73f45f0f93c85a37ea9b829
frkodes <frkalderon@gmail.com>
2018-08-31 18:36:31 -0500

    Adding the livescore link to the homepage

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,6 +4,7 @@
 			<div class="skull-container"><img class="skull" src="images/calavera_new.svg" alt="calavera PISTONES"></div>
 		</div>
 		<div class="row">
+			
 			<div class="col-sm-4 col-md-3 up-this-one">
 				<div class="follow-us">
 					<img src="images/siguenos_new.svg" alt="siguenos en redes sociales">
@@ -14,6 +15,7 @@
 					<li><a target="_blank" title="Contáctanos" href="mailto:info@pistonesgarage.com" class="icon-f"></a></li>
 				</ul>
 			</div>
+
 			<div class="col-sm-4 col-md-3 col-sm-offset-4 col-md-offset-6 up-this-one">
 				<div class="eleanor">
 					<a href="http://www.grupoeleanor.com" title="Ir al sitio de Grupo eleanor" target="_blank"><img src="images/eleanor.svg" alt="Logo eleanor"></a>
@@ -25,6 +27,7 @@
 						Aviso de privacidad <a href="pdf/privacidad-pistones-hmo.pdf" target="_blank" title="Ver aviso de privacidad Hermosillo">Hmo</a> / <a href="pdf/aviso_de_privacidad_pistones.pdf" target="_blank" title="Ver aviso de privacidad Guadalajara">Gdl</a>
 					</p>
 					<p>Desarrollado por <a target="_blank" href="http://www.blueterrier.mx" title="Ver sitio web del studio">Blue Terrier</a></p>
+					<p>Live scores by <a style="color: white" href="http://www.livescore.in/es/" title="Livescore.in" target="_blank">Livescore.in</a></p>
 				</div>
 			</div>
 		</div>
